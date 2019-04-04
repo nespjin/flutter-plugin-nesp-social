6f82f029028abaea0795d674643463b25a6bf2cd
靳兆鲁 <1756404649@qq.com>
2019-04-04 15:36:48 +0800

    -h

--- upload.sh
+++ upload.sh
@@ -2,6 +2,15 @@
 LOCAL_PATH=$(cd `dirname $0`; pwd)
 cd $LOCAL_PATH
 
+
+
+if [ ["$1"=""] || ["$1"="-h"] ];
+then
+echo -e "\033[背景颜色;字体颜色m字符串\033[0m"
+
+fi
+
+
 git add --all 
 git commit -am $1
 git push origin $2
