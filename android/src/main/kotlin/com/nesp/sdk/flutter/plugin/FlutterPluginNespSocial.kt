68e85daa77a64c61aa65d6ce73f70f1c23a962db
靳兆鲁 <1756404649@qq.com>
2019-07-26 11:27:22 +0800

    Update FlutterPluginNespSocial.kt

--- android/src/main/kotlin/com/nesp/sdk/flutter/plugin/FlutterPluginNespSocial.kt
+++ android/src/main/kotlin/com/nesp/sdk/flutter/plugin/FlutterPluginNespSocial.kt
@@ -110,9 +110,9 @@ class FlutterPluginNespSocial : MethodCallHandler {
         val intent = Intent()
         intent.data = Uri.parse("mqqopensdkapi://bizAgent/qm/qr?url=http%3A%2F%2Fqm.qq.com%2Fcgi-bin%2Fqm%2Fqr%3Ffrom%3Dapp%26p%3Dandroid%26k%3D$key")
         // 此Flag可根据具体产品需要自定义，如设置，则在加群界面按返回，返回手Q主界面，不设置，按返回会返回到呼起产品界面
-//        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         return try {
-            context?.startActivity(intent)
+            context!!.startActivity(intent)
             true
         } catch (e: Exception) {
             // 未安装手Q或安装的版本不支持
@@ -133,7 +133,7 @@ class FlutterPluginNespSocial : MethodCallHandler {
 
         val intent = Intent()
         val componentName = ComponentName(weiboPackageName, "com.sina.weibo.page.ProfileInfoActivity")
-//        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
         intent.component = componentName
         intent.putExtra("uid", uidValue)
         context!!.startActivity(intent)
