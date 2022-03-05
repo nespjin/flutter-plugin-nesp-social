e19342e8639f6cd1596ad43d72c54c1adb42298e
luomo-pro <1402440758@qq.com>
2022-03-05 20:20:42 +0800

    Add null safety

--- lib/flutter_plugin_nesp_social.dart
+++ lib/flutter_plugin_nesp_social.dart
@@ -3,12 +3,13 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class FlutterNespSocial {
-
   static const MethodChannel _channel =
       const MethodChannel('flutter_plugin_nesp_social');
 
   static Future<bool> joinQQGroup(
-      {String androidKey, String iosKey, String groupUin}) async {
+      {String androidKey = "",
+      String iosKey = "",
+      String groupUin = ""}) async {
     final bool isSuccess = await _channel.invokeMethod('joinQQGroup', {
       "androidKey": androidKey,
       "iosKey": iosKey,
@@ -18,7 +19,7 @@ class FlutterNespSocial {
   }
 
   static Future<bool> openWeiboUser({
-    String uid,
+    String uid = "",
   }) async {
     final bool isSuccess = await _channel.invokeMethod('openWeiboUser', {
       "uid": uid,
@@ -27,7 +28,7 @@ class FlutterNespSocial {
   }
 
   static Future<bool> joinQQFriend({
-    String qqFriendNumber,
+    String qqFriendNumber = "",
   }) async {
     final bool isSuccess = await _channel.invokeMethod('joinQQFriend', {
       "qqFriendNumber": qqFriendNumber,
@@ -36,9 +37,9 @@ class FlutterNespSocial {
   }
 
   static Future<bool> openOtherApp({
-    String androidPackageName,
-    String androidClassName,
-    String iosAppUrl,
+    String androidPackageName = "",
+    String androidClassName = "",
+    String iosAppUrl = "",
   }) async {
     final bool isSuccess = await _channel.invokeMethod('openOtherApp', {
       "androidPackageName": androidPackageName,
